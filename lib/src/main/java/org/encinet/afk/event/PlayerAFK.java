d5eb7643a0e1abe991d06635a3042c78925fbb6a
ObcbO <m17089494917@163.com>
2022-12-19 11:20:29 +0800

    免伤害

--- lib/src/main/java/org/encinet/afk/event/PlayerAFK.java
+++ lib/src/main/java/org/encinet/afk/event/PlayerAFK.java
@@ -4,9 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.encinet.afk.Config;
 import org.encinet.afk.unit.dispose;
 import org.purpurmc.purpur.event.PlayerAFKEvent;
+
+import static org.bukkit.entity.EntityType.PLAYER;
 
 public class PlayerAFK implements Listener {
     @EventHandler
@@ -23,6 +26,13 @@ public class PlayerAFK implements Listener {
             if (Config.broadcastBackEnable) {
                 e.setBroadcastMsg(dispose.papi(player, dispose.randomList(Config.broadcastBackMessages)));
             }
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent e) {
+        if (!(e.getEntityType() == PLAYER)) {
+            e.setCancelled(true);
         }
     }
 }
